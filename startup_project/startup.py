b7007e52f34a79c6aaab13007c22bbf8dc937bfd
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2020-04-04 21:37:23 +0530

    startup_project - Create startup.py

---
+++ startup_project/startup.py
@@ -0,0 +1,28 @@
+class Startup:
+    def __init__(self):
+        self.name = ""
+        self.salaries = {}
+        self.funding = 0
+        self.employees = []
+
+    def is_valid_title(self, title):
+        return False
+
+    def hire(self, name, title):
+        pass
+
+    def size(self):
+        return 0
+
+    def payday(self):
+        pass
+
+    def average_salary(self):
+        return 0
+
+    def close(self):
+        pass
+
+    def acquire(self):
+        pass
+    
