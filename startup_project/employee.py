31fd4f00f8e67a686d1c827ebf430b4894c0b9ec
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2020-04-07 21:32:04 +0530

    startup_project - Create employee.py

---
+++ startup_project/employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, name, title, earnings):
+        self.name = name
+        self.title = title
+        self.earnings = earnings
+
+    def pay(self, amount):
+        pass
+        
