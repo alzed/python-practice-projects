6e7e401ec70b5958f6af4b53f6605fae319d4750
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2020-05-28 00:35:44 +0530

    startup project - Add methods for startup

--- startup_project/startup.py
+++ startup_project/startup.py
@@ -1,28 +1,44 @@
+from employee import Employee
+
 class Startup:
-    def __init__(self):
-        self.name = ""
-        self.salaries = {}
-        self.funding = 0
+    def __init__(self, name, funding, salaries):
+        self.name = name
+        self.salaries = salaries
+        self.funding = funding
         self.employees = []
 
     def is_valid_title(self, title):
-        return False
+        return title in self.salaries
 
     def hire(self, name, title):
-        pass
+        if self.is_valid_title(title):
+            self.employees.append(Employee(name, title))
 
     def size(self):
-        return 0
+        return len(self.employees)
 
     def payday(self):
-        pass
+        for employee in self.employees:
+            salary = self.salaries[employee.title]
+            employee.pay(salary)
+            self.funding -= salary
 
     def average_salary(self):
-        return 0
+        sum_salaries = sum([self.salaries[employee.title] for employee in self.employees])
+        return sum_salaries/len(self.employees)
 
     def close(self):
-        pass
+        self.employees = []
+        self.funding = 0
 
-    def acquire(self):
-        pass
+    def acquire(self, startup):
+        self.funding += startup.funding
+        for title, salary in startup.salaries.items():
+            if title not in self.salaries:
+                self.salaries[title] = salary
+
+        for employee in startup.employees:
+            self.hire(employee.name, employee.title)
+
+        startup.close()
     
