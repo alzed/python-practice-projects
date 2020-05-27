c96af67e46da80d15996bef49f5f70e9827a691b
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2020-05-27 22:24:00 +0530

    Tictactoe - Create main.py

---
+++ python-games/tictactoe/main.py
@@ -0,0 +1,48 @@
+from tictactoe import Gameboard
+
+def player():
+    print('Enter points: ', end=' ')
+    r, c  = map(int, input().split())
+    if game.check_pos(r, c):
+        game.set_pos(xo, r, c)
+    game.display_board()
+
+def computer():
+    game.play()
+    game.display_board()
+
+game = Gameboard()
+print('Choose x or o: ', end=' ')
+xo = input()
+x = game.choose_symbol(xo)
+game.display_board()
+if x:
+    player()
+    for i in range(4):
+        computer()
+        result = game.check_wins()
+        if result in ['won', 'lost']:
+            print(f'You {result}. Game over')
+            break
+        player()
+        result = game.check_wins()
+        if result in ['won', 'lost']:
+            print(f'You {result}. Game over')
+            break
+    else:
+        print('Game draw')
+else:
+    computer()
+    for i in range(4):
+        player()
+        result = game.check_wins()
+        if result in ['won', 'lost']:
+            print(f'You {result}. Game over')
+            break
+        computer()
+        result = game.check_wins()
+        if result in ['won', 'lost']:
+            print(f'You {result}. Game over')
+            break
+    else:
+        print('Game draw')
