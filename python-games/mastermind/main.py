149ba65deaec95ad3b806d446087085aeebfc71a
alzed <madhumithap.ravishankar@gmail.com>
2020-06-04 23:55:46 +0530

    Update main.py

--- python-games/mastermind/main.py
+++ python-games/mastermind/main.py
@@ -4,9 +4,12 @@ print("Enter the size of the game:", end="  ")
 size = int(input())
 
 mm = Mastermind(size)
+mm.declare_pegs()
 
-game = True
-while game:
+game = mm.game_over
+while not game:
     guess = mm.ask_user()
     mm.print_matches(guess)
+    game = mm.game_over
+print("You won")
     
