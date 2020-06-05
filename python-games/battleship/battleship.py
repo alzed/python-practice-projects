d75c27d79c524759fdc3be81e66f4dcebafc9393
alzed <madhumithap.ravishankar@gmail.com>
2020-06-06 00:47:52 +0530

    Update battleship.py

--- python-games/battleship/battleship.py
+++ python-games/battleship/battleship.py
@@ -2,23 +2,33 @@ from board import Board
 from player import Player
 
 class Battleship:
-    def __init__(self):
+    def __init__(self, size):
         self.player = Player()
-        self.board = Board()
-        self.remaining_guesses = 0
+        self.board = Board(size)
+        self.remaining_guesses = size + 1 if size > 2 else size
 
     def start_game(self):
-        pass
+        self.board.print_grid()
+        self.ships = self.board.place_random_ships()
+        self.won = 0
 
-    def is_win(self):
-        return False
+    def turn(self, position):
+        pos = list(map(int, position.split(',')))
+        attacked = self.board.attack(pos)
+        if attacked == -1:
+            self.remaining_guesses -= 1
+            self.board.print_grid()
+        elif attacked == 1:
+            self.won += 1
+            self.board.print_grid()
+        else:
+            raise ValueError('Repeated guess')
+
+    def is_won(self):
+        return self.won == self.ships
 
     def is_lost(self):
-        return False
+        return self.remaining_guesses == 0
 
     def is_game_over(self):
-        return False
-
-    def turn(self):
-        pass
-
+        return self.is_won or self.is_lost
