6e61e90f7c8491abf4224f24d88fb4d97e2548ba
alzed <madhumithap.ravishankar@gmail.com>
2020-06-07 21:38:28 +0530

    Update player.py
    Add get_move

--- python-games/battleship/player.py
+++ python-games/battleship/player.py
@@ -3,5 +3,11 @@ class Player:
         pass
 
     def get_move(self):
-        pass
+        print("Enter the position (space separated):", end="  ")
+        coordinates = input()
+        pos = tuple(map(lambda p: int(p)-1, coordinates.split()))
+        if not len(pos) == 2:
+            print("Invalid position")
+            return self.get_move()
+        return pos
     
