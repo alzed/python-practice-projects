65e3b82c5d27f219414a0d83aebe7017c9a933a9
alzed <madhumithap.ravishankar@gmail.com>
2020-06-07 21:41:27 +0530

    Update main.py
    Add game logic

--- python-games/battleship/main.py
+++ python-games/battleship/main.py
@@ -5,4 +5,12 @@ print("Enter the size of grid:", end="  ")
 size = int(input())
 
 bs = Battleship(size)
+bs.start_game()
 
+while not bs.is_game_over():
+    bs.turn()
+
+if bs.is_won():
+    print("You won")
+else:
+    print("You lost")
