a3d38f0fcca4f65aad8159f7bfa506d6b6a06ca3
alzed <madhumithap.ravishankar@gmail.com>
2020-06-07 21:41:11 +0530

    Update battleship.py
    Handle invalid input

--- python-games/battleship/battleship.py
+++ python-games/battleship/battleship.py
@@ -5,30 +5,34 @@ class Battleship:
     def __init__(self, size):
         self.player = Player()
         self.board = Board(size)
-        self.remaining_guesses = size + 1 if size > 2 else size
+        self.remaining_guesses = self.board.num_ships() + 2
 
     def start_game(self):
         self.board.print_grid()
-        self.ships = self.board.place_random_ships()
-        self.won = 0
+        self.board.place_random_ships()
 
-    def turn(self, position):
-        pos = list(map(int, position.split(',')))
-        attacked = self.board.attack(pos)
-        if attacked == -1:
-            self.remaining_guesses -= 1
-            self.board.print_grid()
-        elif attacked == 1:
-            self.won += 1
-            self.board.print_grid()
+    def turn(self):
+        position = self.player.get_move()
+        if 0 <= position[0]*position[1] <= (self.board.size-1)**2:
+            attacked = self.board.attack(position)
+            if attacked == -1:
+                self.remaining_guesses -= 1
+                self.board.print_grid()
+            elif attacked == 1:
+                print("You sunk my battleship!")
+                self.board.print_grid()
+            else:
+                print('Repeated guess')
+                self.turn()
         else:
-            raise ValueError('Repeated guess')
+            print("Invalid position")
+            self.turn()
 
     def is_won(self):
-        return self.won == self.ships
+        return self.board.num_ships() == 0
 
     def is_lost(self):
         return self.remaining_guesses == 0
 
     def is_game_over(self):
-        return self.is_won or self.is_lost
+        return self.is_won() or self.is_lost()
