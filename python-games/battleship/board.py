110374170580f015fda2367c05add5d6457ca588
alzed <madhumithap.ravishankar@gmail.com>
2020-06-07 21:40:19 +0530

    Update board.py
    Change duplicate ships grid to a list of positions and bug fix

--- python-games/battleship/board.py
+++ python-games/battleship/board.py
@@ -5,31 +5,31 @@ from copy import deepcopy
 class Board:
     def __init__(self, size):
         self.size = size
-        self.grid = [['-' for i in size] for j in size]
+        self.grid = [['-' for i in range(size)] for j in range(size)]
+        self.ships = int(size**2 * 0.25)
 
     def print_grid(self):
         print(tabulate(self.grid, tablefmt="pretty"))            
 
-    def place_random_ships(self):
-        self.ships_grid = deepcopy(self.grid)
-        ships = 1 if self.size == 2 else self.size
-        ships_size = deepcopy(ships)
-        while ships_size == 0:
-            x, y = choices(range(self.size), k=2)
-            if self.ships_grid[x][y] == '-':
-                self.ships_grid[x][y] = 'H'
-                ships_size -= 1
-        return ships
+    def num_ships(self):
+        return self.ships 
 
-    def hidden_ships_grid(self):
-        print(tabulate(self.ships_grid, tablefmt="pretty"))
+    def place_random_ships(self):
+        self.ships_grid = []
+        ships_size = deepcopy(self.ships)
+        while ships_size:
+            pos = tuple(choices(range(self.size), k=2))
+            if pos not in self.ships_grid:
+                self.ships_grid.append(pos)
+                ships_size -= 1
 
     def attack(self, position):
         x, y = position
         if self.grid[x][y] == 'X' or self.grid[x][y] == 'O':
             return 0
-        elif self.ships_grid[x][y] == 'H':
+        elif position in self.ships_grid:
             self.grid[x][y] = 'O'
+            self.ships -= 1
             return 1
         else:
             self.grid[x][y] = 'X'
